5a0d19a72a39708d15f63fcc65e82c6d41ee853f
Emil <codewithemil@gmail.com>
2022-11-23 23:13:21 -0600

    Version 2 complete! I finished adding the buttons and the counter works perfectly. I tried doing a copy-paste system when the user interacts with the box where the number is stored in, but it didn't really work...

--- src/components/display/display.js
+++ src/components/display/display.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom";
 
 
 const Display = () => {
-    return(
-        <h1>Hello from Display.js!</h1>
+    //Hooks
+    let [count, setCount] = useState(0);
+
+    //Functions
+    const increaseCount = () => setCount(count++);
+    const decreaseCount = () => setCount(count--);
+
+    const handleClick = (e) => {
+        e.target.value.document.prototype.execCommand("copy");
+    }
+
+    return (
+        <div className="display--container">
+            <section className="display--button--container">
+                <button className="display--button display--decrease" onClick = {decreaseCount}>-</button>
+            </section>
+
+            <section className="display--counter--container" onClick = {() => handleClick}>
+                <div className="display--counter">{count}</div>
+            </section>
+
+            <section className="display--button--container">
+                <button className="display--button display--increase" onClick = {increaseCount}>+</button>
+            </section>
+        </div>
     );
 };
 
